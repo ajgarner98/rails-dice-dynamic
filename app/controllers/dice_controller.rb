f49a8c0d2d1bc962f290910a9b644fde34b553ea
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-26 17:27:24 +0000

    Made dynamic rolls work properly need home page

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,11 +1,11 @@
 class DiceController < ApplicationController
   def roll
-    @num_dice = params.fetch(number_of_dice).to_i
-    @sides = params.fetch(how_many_sides).to_i
-    @dicerolls
+    @num_dice = params.fetch(:number_of_dice).to_i
+    @sides = params.fetch(:how_many_sides).to_i
+    @dicerolls = []
     @num_dice.times do
       @die = rand(1..@sides)
-      @dicerolls.push(die)
+      @dicerolls.push(@die)
     end
 
 
