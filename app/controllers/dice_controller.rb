3da8a18904c8ebdde7bdd6422cab91d962b57986
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-26 17:08:21 +0000

    made part of the logic for the flexible dice

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,7 +1,15 @@
-class DiceController
-  def dice
+class DiceController < ApplicationController
+  def roll
+    @num_dice = params.fetch(number_of_dice).to_i
+    @sides = params.fetch(how_many_sides).to_i
+    @dicerolls
+    @num_dice.times do
+      @die = rand(1..@sides)
+      @dicerolls.push(die)
+    end
 
-    render(:template=> "dicerolls/roll_flexbile")
+
+    render({:template => "dicerolls/roll_flexible"})
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get("/", {:controller => "dice", :action => "roll"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "roll"})
 end
